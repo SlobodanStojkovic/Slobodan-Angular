a8751ce489b91484dc2b3f6cb4d1ba28c8be8b8d
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-06-28 17:53:28 +0200

    Add Angular Intro

---
+++ angular-intro.js
@@ -0,0 +1,13 @@
+/* 
+Angular is a JavaScript Framework which allows you to create reactive Single-Page-Applications (SPAs).
+
+ANGULAR Versioning
+- AngularJS (Angular 1)
+- Angular 2 (completely Re-Writen)
+- Angular 4 (Angular 3 was skipped)
+- Angular 10, 11, 12, ... > New version every 6 months, but is very backward compatible
+
+TO START
+install Angular CLI - a command line interface for Angular
+To install first project `ng new my-first-angular-app --no-strict`
+*/
