4ab09dcc73062b91356e214d0b122e765956dc5d
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-06-30 18:53:58 +0200

    Add Creating New Components

--- angular-intro.js
+++ angular-intro.js
@@ -10,4 +10,29 @@ ANGULAR Versioning
 TO START
 install Angular CLI - a command line interface for Angular
 To install first project `ng new my-first-angular-app --no-strict`
+
+The first code that is being executed is the code inside src/app/main.ts
+
+CREATING NEW COMPONENTS
+Manually - create new folder inside app folder and inside of it component-name.component.ts file which will contain inside it:
+
+import { Component } from "@angular/core"
+
+// this is decorator with configuration
+@Component({
+  selector: "app-warning-alert",
+  template:`<p>THis is a worning</p>` OR templateURL: with url to "./warning-alert.component.html",
+  styleUrls: ["./warning-alert.component.css"]
+})
+export class ComponentNameComponent {
+
+}
+
+and in app.component.html insert <app-warning-alert></app-warning-alert>
+in app.module.ts we need to add in declarations array our new Component and import it at top
+
+CREATE NEW COMPONENTS via Angular CLI
+In terminal we run `ng generate component component-name` or short `ng g c component-name`, then we output them in app.component.html
+
+
 */
