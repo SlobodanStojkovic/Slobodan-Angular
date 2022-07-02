14000b51c4fc8f6a757910c055eec17f37cef5f6
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-07-02 22:31:25 +0200

    Add Directives

--- angular-intro.js
+++ angular-intro.js
@@ -44,4 +44,34 @@ Two-Way-Binding ([(ngModel)]="data")
 
 For Two-Way-Binding to work we need to enable ngModel directive. This is done by adding the FormsModule to the imports[] array in the AppModule. We then also have to add the import from @angular/forms in the app.module.ts file. import { FormsModule } from "@angular/forms"
 
+==========
+DIRECTIVES are INSTRUCTIONS in the DOM. Components are directives with a template. 
+==========
+
+Custom directive:
+<p appTurnGreen>Recieves a green background!</p>
+
+@Directive({
+  selector: "[appTurnGreen]"
+})
+export class TurnGreenDirective {
+  ...
+}
+
+The ngIf Directive (star is required because ngIf is a structural directive, it changes the structure of our DOM, it either adds the element or not)
+<p *ngIf="serverCreated; else noServer"<Server was created, server name is {{ serverName }} </p>
+
+<ng-template #noServer> <p>No server was created! </p> </ng-template>   where # is local reference to add paragraph conditionally
+
+
+STYLING ELEMENTS DYNAMICLY with ngStyle
+========================================
+<p [ngStyle]="{backgroundColor: getColor()}" [ngClass]="{online: serverStatus === "online"}"> {{ "Server "}} with ID {{ serverId }} is {{ getServerSTatus() }} </p>
+
+ngClass only adds CSS class if a certain condition is true > Built in ATTRIBUTE DIRECTIVE
+
+
+OUTPUTING LISTS with ngFor
+===========================
+<app-server *ngFor="let server of servers"></app-server>
 */
